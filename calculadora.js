dfd32922c62e9940c7b64e3f95bc356712fa3479
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-07 10:46:38 +0000

    Deploy ojgarciab/diferencia-de-tiempo-en-javascript to github.com/ojgarciab/diferencia-de-tiempo-en-javascript.git:gh-pages

--- calculadora.js
+++ calculadora.js
@@ -1,33 +1,33 @@
-document.addEventListener('DOMContentLoaded', (evento) => {
+document.addEventListener("DOMContentLoaded", (evento) => {
     /* Almacenamos los elementos más usados del DOM */
-    let ini_fecha = document.getElementById('ini-fecha');
-    let ini_hora = document.getElementById('ini-hora');
-    let fin_fecha = document.getElementById('fin-fecha');
-    let fin_hora = document.getElementById('fin-hora');
-    let calcular = document.getElementById('calcular');
-    let actual = document.getElementById('actual');
+    let iniFecha = document.getElementById("ini-fecha");
+    let iniHora = document.getElementById("ini-hora");
+    let finFecha = document.getElementById("fin-fecha");
+    let finHora = document.getElementById("fin-hora");
+    let calcular = document.getElementById("calcular");
+    let actual = document.getElementById("actual");
     /* Agregamos la función de reinicio de valores a la hora actual cuando pulsemos el botón */
-    actual.addEventListener('click', () => {
+    actual.addEventListener("click", () => {
         /* Obtenemos la fecha actual y calculamos fecha y hora */
         let ahora = new Date();
         let fecha = ahora.toISOString().substring(0, 10);
         let hora = ahora.toISOString().substring(11, 19);
         /* Cargamos los valores iniciales de los campos */
-        ini_fecha.value = fecha;
-        ini_hora.value = hora;
-        fin_fecha.value = fecha;
-        fin_hora.value = hora;
+        iniFecha.value = fecha;
+        iniHora.value = hora;
+        finFecha.value = fecha;
+        finHora.value = hora;
     });
     /* Al cargar la página realizamos un clic inicial */
     actual.click();
     /* Agregamos la función de cálculo cuando pulsemos el botón */
-    calcular.addEventListener('click', () => {
+    calcular.addEventListener("click", () => {
         /* Convertimos los campos en fechas completas */
         let ini = new Date(
-            [ ini_fecha.value, ini_hora.value ].join(' ')
+            [ iniFecha.value, iniHora.value ].join(" ")
         );
         let fin = new Date(
-            [ fin_fecha.value, fin_hora.value ].join(' ')
+            [ finFecha.value, finHora.value ].join(" ")
         );
         /* Calculamos la diferencia en segundos, minutos y horas */
         let segundos = Math.abs(ini - fin) / 1000;
@@ -36,10 +36,10 @@ document.addEventListener('DOMContentLoaded', (evento) => {
         let horas = Math.floor(minutos / 60);
         minutos %= 60;
         let resultado = [
-            ('0' + horas).substr(-2),
-            ('0' + minutos).substr(-2),
-            ('0' + segundos).substr(-2)
-        ].join(':');
-        document.getElementById('res-hora').value = resultado;
+            ("0" + horas).substr(-2),
+            ("0" + minutos).substr(-2),
+            ("0" + segundos).substr(-2)
+        ].join(":");
+        document.getElementById("res-hora").value = resultado;
     });
 });
